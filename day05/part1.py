f4471ca1a9e71b3167053f5be11ecbf150167632
Christian Kehrmann <christian.kehrmann@advantest.com>
2021-12-23 19:49:28 +0100

    day 05 part1 cleanup

--- day05/part1.py
+++ day05/part1.py
@@ -3,37 +3,7 @@
 input_data_file = "part1.data"
 
 
-def slurp_input(input_file):
-    """
-    Read the input file.
-    :param input_file:
-    :return: A list of sets. Each set represents the given answers in a group.
-    """
-    f = open(input_file, "r")
-    lines = f.readlines()
-    for line in lines:
-        links, foo, rechts = line.split()
-        start_x, start_y = links.split(',')
-        ziel_x, ziel_y = rechts.split(',')
-        if start_x != ziel_x and start_y != ziel_y:
-            next()
-
-
-
-
-    print("Zeile: {}".format(line))
-    print("Links: {}".format(links))
-    print("Rechts: {}".format(rechts))
-
-
-
-
-
-    return "123"
-
-
 def main():
-
     # uncomment the next line to read the input data from the test file
     # input_data_file = "part1_test.data"
 
@@ -58,7 +28,6 @@ def main():
         if ziel_y > max_y:
             max_y = ziel_y
 
-    # print(max_x, max_y)
     matrix = []
     for i in range(max_x + 1):
         spalte = [0] * (max_y + 1)
@@ -73,15 +42,11 @@ def main():
         ziel_x = int(ziel_x)
         ziel_y = int(ziel_y)
         if start_x == ziel_x:
-            # print("X ist gleich")
-            # print(start_y, ziel_y)
             if start_y > ziel_y:
                 start_y, ziel_y = ziel_y, start_y
             for i in range(start_y, ziel_y + 1):
                 matrix[start_x][i] += 1
         elif start_y == ziel_y:
-            # print("Y ist gleich")
-            # print(start_x, ziel_x)
             if start_x > ziel_x:
                 start_x, ziel_x = ziel_x, start_x
             for i in range(start_x, ziel_x + 1):
@@ -89,24 +54,13 @@ def main():
         else:
             continue
 
-        # print("Matrix:")
-        # print(matrix)
-
     total = 0
     for spalte in matrix:
-        # print(spalte)
         for x in spalte:
             if x > 1:
                 total += 1
 
     print("Total: {}".format(total))
-
-    # print("Zeile: {}".format(line))
-    # print("Links: {}".format(links))
-    # print("Rechts: {}".format(rechts))
-
-    # print("Total: {}".format(total))
-
 
 if __name__ == "__main__":
     main()
